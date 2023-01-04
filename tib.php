d23c53b3a2fed371bd29af6505c58be34e7aa2a0
Dan Dyse <45576905+humpataa@users.noreply.github.com>
2023-01-04 13:17:45 +0100

    Update tib.php

--- tib.php
+++ tib.php
@@ -13,15 +13,6 @@
 	<body>
 <?php
 
-/*
-	$json = '{"query":"{viewer {homes {currentSubscription {priceInfo {current {total energy tax startsAt }}}}}}"}';
-	$json = '{"query":"{name}"}';
-	$json = '{"query":"{viewer {homes {currentSubscription {priceInfo {current {total energy tax startsAt}}}}}}"}';
-	$json = '{"query":"subscription{liveMeasurement(homeId:"2f42cbcd-cd15-4667-89ce-8d83c2e1d22f"){timestamp accumulatedConsumption}}"}';
-	$json = '{"query":"subscription{liveMeasurement(homeId:"2f42cbcd-cd15-4667-89ce-8d83c2e1d22f"){accumulatedConsumption}}"}';
-*/
-	//$json = '{"query":"{viewer {homes {consumption(resolution: HOURLY, last: 100) {nodes {from to cost unitPrice unitPriceVAT consumption consumptionUnit}}}}}"}';
-
 	if (isset($_GET["apikey"])) $apikey = $_GET["apikey"];
 
 	date_default_timezone_set('CET');
@@ -30,7 +21,6 @@
 	$datetime->setTime(0, 0, 0);
 	$day = 0;
 	if (isset($_GET["day"]) && is_numeric($_GET["day"])) $day = $_GET["day"];
-	//echo $day." ".$datetime->format('r')."<br>";
 
 	$datetime->modify(($day<0?'':'+').$day.' day');
 	$cursor = base64_encode($datetime->format('c'));
@@ -42,57 +32,34 @@
 	echo '
 		<a href="?day='.($day-1).'&apikey='.$apikey.'">zurück</a> '.$datetime->format('l, d.m.Y').' <a href="?day='.($day+1).'&apikey='.$apikey.'">vor</a></br></br>
 	';
-	//echo ."</br></br>";
-	//die;
 	
-	$fp = fopen("keyss.txt", "a+");
-	fwrite($fp, $apikey."\n");
-	fclose($fp);
-
-	//file_put_contents("keys.txt", "geht das", FILE_APPEND);
-
 	$json = '{"query":"{viewer {homes {timeZone address {postalCode city}consumption(resolution: HOURLY last: 24 before: \"'.$cursor.'\") {nodes {from to cost unitPrice unitPriceVAT consumption consumptionUnit}}}}}"}';
 
-	# Create a connection
 	$ch = curl_init('https://api.tibber.com/v1-beta/gql');
-	# Setting our options
 	curl_setopt($ch, CURLOPT_URL, 'https://api.tibber.com/v1-beta/gql');
 	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Authorization: Bearer '.$apikey)); 
 	curl_setopt($ch, CURLOPT_POST, true);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-	# Get the response
 	$response = curl_exec($ch);
 	curl_close($ch);
 	
 	//var_dump($response);
 	
-	//Decoding of the JSON data
-	/*
-		$data = json_decode($response);
-		$current = $data->data->viewer->homes[0]; 
-		var_dump($current);
-	*/
-	
 	$data = json_decode($response, true, 512, JSON_OBJECT_AS_ARRAY);
+	
 	//var_dump($data);
 	
 	if (isset($data["data"]["viewer"]["homes"])) {
 		foreach($data["data"]["viewer"]["homes"] as $home) {
-
 			if (isset($home["consumption"]["nodes"])) {
-				//var_dump($home["consumption"]["nodes"]);
 				$max_cost = $max_unitPrice = $max_consumption = 0;
 				$nodes = $home["consumption"]["nodes"];
-
 				foreach($nodes as $node) {
 					if ($node["cost"] > $max_cost) $max_cost = $node["cost"];
 					if ($node["unitPrice"] > $max_unitPrice) $max_unitPrice = $node["unitPrice"];
 					if ($node["consumption"] > $max_consumption) $max_consumption = $node["consumption"];
 				}
-				
-				//echo "</br>maxcost=".$max_cost." maxunitPrice=".$max_unitPrice." max_consumption=".$max_consumption."<br>";
 			}
 		}
 	}
@@ -144,10 +111,10 @@
 			// Cost Kurve
 			echo '<line x1="'.($okey * $stepbreite + $stepbreite / 2 + 50).'" y1="'.($hoehe + 20 - $ocost * $faktorcost).'" x2="'.($key * $stepbreite + $stepbreite / 2 + 50).'" y2="'.($hoehe + 20 - $cost * $faktorcost).'" stroke-width="2" stroke="#AAAAAA" />';
 		}
-*/
 
 		//echo '<text x="5" y="'.($stepbreite / 2).'" font-size="small+1" text-anchor="start" transform="translate('.($key * $stepbreite + 55).','.($hoehe + 20 - $cost * $faktorcost).') rotate(-90)">'.(ceil($cost*100)/100).' € </text>';
-
+*/
+		
 		echo '<text x="5" y="'.($stepbreite / 2).'" font-size="small+1" text-anchor="start" transform="translate('.($key * $stepbreite + 55).', 60) rotate(-90)">'.(ceil($cost*100)/100).' € </text>';
 
 		if (isset($ounitPrice)) {
@@ -163,28 +130,14 @@
 		echo '<text x="-5" y="'.($stepbreite / 2).'" font-size="small+1" text-anchor="end" transform="translate('.($key * $stepbreite + 55).','.($hoehe + 20).') rotate(-90)" fill="#00AA00">'.$key.'</text>';
 		
 		$sum_consumption += $consumption;
-		//$sum_cost += ceil($cost*100)/100;
 		$sum_cost += $cost;
 		$sum_unitPrice += $unitPrice;
 	}
 	
 	echo "</svg></br>";
-	
 	echo "Gesamtverbrauch: ".$sum_consumption." kWh</br>";
 	echo "Gesamtkosten: ".(ceil($sum_cost * 100) / 100)." Euro</br>";
-	
 	echo '<a target="_new" href="tibexport.php?day='.$day.'&apikey='.$apikey.'">CSV-Download</a>';
-
-	//echo "Schnitt unitPrice: ".($sum_unitPrice / sizeof($nodes))." Euro</br>";
-
-	//$total = $current->currentSubscription->priceInfo->current->total; 
-	//var_dump($total); 
-	
-	//Convert KR to Øre
-	//$pris = floatval($total) * floatval(100);
-	
-	//Write Energy price Incl Tax to Float 
-	//setvalue  (35230, $pris) ;
 ?>
 	</body>
 </html>
